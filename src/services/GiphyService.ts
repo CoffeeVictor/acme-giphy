f078a7da38135ee1eb97ded612a6cfa0b617ff78
CoffeeVictor <victor.jgfsilva@gmail.com>
2023-05-09 19:31:22 -0300

    Creates GiphyService

---
+++ src/services/GiphyService.ts
@@ -0,0 +1,29 @@
+export interface IGiphyAPISearchResponse {
+  data: IGiphyAPIResultData[];
+}
+
+export interface IGiphyAPIResultData {
+  downsized_small: string;
+  fixed_width: string;
+}
+
+export class GiphyService {
+  private apiKey: string;
+  private giphySearchEnpoint: string;
+
+  constructor() {
+    this.apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY as string;
+    this.giphySearchEnpoint = "http://api.giphy.com/v1/gifs/search";
+  }
+
+  public async search(input: string) {
+    const response = await fetch(
+      this.giphySearchEnpoint + "?api_key=" + this.apiKey + "&q=" + input,
+      {
+        method: "GET",
+      }
+    );
+
+    return (await response.json()) as IGiphyAPISearchResponse;
+  }
+}
